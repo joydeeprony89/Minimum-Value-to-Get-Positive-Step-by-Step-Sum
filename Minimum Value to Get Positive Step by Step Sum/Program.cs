9bdb34c3827c2ed251b6d4755f98ba8b51922d57
Jaydeep Shil <ED8A@eurofins.com>
2020-12-10 10:54:13 +0530

    Add project files.

---
+++ Minimum Value to Get Positive Step by Step Sum/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Minimum_Value_to_Get_Positive_Step_by_Step_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nums = new int[] {-3, 2, -3 , 4 , 2 };
+            Console.WriteLine(MinStartValue(nums));
+        }
+
+        static int MinStartValue(int[] nums)
+        {
+            int sum = 0;
+            int max = 1;
+            foreach(int n in nums)
+            {
+                sum += n;
+                if(sum < 0)
+                {
+                    max = Math.Max(max, 1 + Math.Abs(sum));
+                }
+            }
+            return max;
+        }
+    }
+}
